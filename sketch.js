3d69d377d45d2e36a9df1c3e7af9ba9be6d3132d
Niks <wearedsgnd@gmail.com>
2024-06-28 14:50:53 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function videoReady() {
 }
 
 function init() {
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 5000; i++) {
     let x = random(width);
     let y = random(height);
     let col = video.get(x, y);
